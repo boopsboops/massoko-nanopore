f80fdde428a7db0d91bfac10e7c3cefac1dd7581
rupert@it031740 <rupertcollins@gmail.com>
2019-12-06 20:42:24 +0000

    added cat

--- scripts/guppy.sh
+++ scripts/guppy.sh
@@ -5,11 +5,18 @@
 # sudo apt install ont-guppy-cpu
 
 # set working dirs
-WDIR="../temp-local-only/data/Massoko_Shallow1/3MA/20191129_1341_MN32297_ABH252_2cedd62b"
-mkdir "$WDIR"/fastq
+WDIR="../temp-local-only/data/Massoko"
+RUN="3mA"
+RUN="3mB"
+RUN="22mA"
+RUN="22mB"
+RUN="22mBv2"
+
+# make a fastq dir
+mkdir "$WDIR"/"$RUN"/fastq
 #rm -r "$WDIR"/fastq
 
-# check version (3.4.1+ad4f8b9) and configs
+# check version (3.4.3+f4fc735) and configs
 guppy_basecaller -h
 guppy_basecaller --version
 guppy_basecaller --print_workflows
@@ -18,11 +25,14 @@ guppy_basecaller --print_workflows | grep "FLO-FLG001" | grep "SQK-LSK109"
 # https://community.nanoporetech.com/protocols/Guppy-protocol/v/gpb_2003_v1_revo_14dec2018/guppy-basecaller-and-guppy-basecaller-server
 
 # run guppy (high quality)
-guppy_basecaller --input_path "$WDIR"/fast5 --save_path "$WDIR"/fastq --flowcell FLO-FLG001 --kit SQK-LSK109 --qscore_filtering --min_qscore 7 --records_per_fastq 4000 --compress_fastq --progress_stats_frequency 60 --num_callers 1 --cpu_threads_per_caller 7
+guppy_basecaller --input_path "$WDIR"/"$RUN"/fast5 --save_path "$WDIR"/"$RUN"/fastq --flowcell FLO-FLG001 --kit SQK-LSK109 --qscore_filtering --min_qscore 7 --records_per_fastq 4000 --compress_fastq --progress_stats_frequency 60 --num_callers 1 --cpu_threads_per_caller 7
 
 # run guppy (low quality) see https://community.nanoporetech.com/posts/help-with-guppy-cpu-versi
-guppy_basecaller --input_path "$WDIR"/fast5 --save_path "$WDIR"/fastq --config dna_r9.4.1_450bps_fast.cfg --qscore_filtering --min_qscore 7 --records_per_fastq 4000 --compress_fastq --progress_stats_frequency 60 --num_callers 1 --cpu_threads_per_caller 7
+guppy_basecaller --input_path "$WDIR"/"$RUN"/fast5 --save_path "$WDIR"/"$RUN"/fastq --config dna_r9.4.1_450bps_fast.cfg --qscore_filtering --min_qscore 7 --records_per_fastq 4000 --compress_fastq --progress_stats_frequency 60 --num_callers 1 --cpu_threads_per_caller 7
 
 # info 
 # R9.4.1 nanopores - 1D experiments
 # are 'FLO-FLGOP1' the same as 'FLO-FLG001'? - received FLO-FLGOP1 first, then FLO-FLG001
+
+# cat a fastq
+cat "$WDIR"/"$RUN"/fastq/pass/*.fastq.gz > "$WDIR"/"$RUN"/fastq/"$RUN".fastq.gz
